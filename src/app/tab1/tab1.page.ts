6e4e9505866f1b14763d2f44f97db1f1f3c1343b
hulpoialexandra <hulpoialexandra08@gmail.com>
2021-01-18 17:50:07 +0200

    List modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -18,29 +18,6 @@ export class Tab1Page {
   ngOnInit(){
     this.server=new Service();
     this.list=this.server.getItems();
-    this.list.forEach(e=>{
-      var item=document.createElement('ion-item');
-      var label=document.createElement('ion-label');
-      label.innerHTML=e.text;
-      item.appendChild(label);
 
-      var buttons=document.createElement('ion-buttons');
-      buttons.slot='end';
-
-      var btnFav=document.createElement('ion-button');
-      var fav=document.createElement('ion-icon');
-      fav.name='heart';
-      btnFav.appendChild(fav);
-      buttons.appendChild(btnFav)
-
-      var btnCart=document.createElement('ion-button');
-      var cart=document.createElement('ion-icon');
-      cart.name='cart';
-      btnCart.appendChild(cart);
-      buttons.appendChild(btnCart)
-
-      item.appendChild(buttons);
-      document.getElementById('list').appendChild(item);
-    })
   }
 }
